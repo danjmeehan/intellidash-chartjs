3b04e6f722a977832ea37f45df399954072df64c
Dan Meehan <Dan Meehan>
2016-02-22 20:45:53 -0600

    customers page

--- assets/js/customers.js
+++ assets/js/customers.js
@@ -1,1 +1,15 @@
-// Delete this comment: How do we make sure our code runs only after the page loads?
+$(document).ready(function(){
+	$.get('https://www.batchacademy.com/api/wdfne/dummy/intellidash', function(data){
+		for(var i = 0; i < data.length; i++){
+			
+			var newRow = $('<tr/>');
+			var newData = $('<td/>');
+			
+			$(newRow).append('<td>' + data[i].id, '<td>' + data[i].person.first_name + ' ' + data[i].person.last_name, '<td>' + data[i].person.email, 
+			'<td>' + data[i].person.location.city,'<td>' + data[i].person.location.state,'<td>' + data[i].person.race,
+			'<td>' + data[i].person.gender);
+			$('tbody').append(newRow);
+		}
+		fixSidebar();
+	});
+});
